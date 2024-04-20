af5462aded7c8b8cbf2515327f975f9f9ad4cf16
rupeshrj123 <rupeshjainrj999@gmail.com>
2024-04-20 18:02:07 +0530

    testing change

---
+++ src/testpackage/test.java
@@ -0,0 +1,15 @@
+package testpackage;
+
+public class test {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("     Test");
+		
+		
+		
+		
+	}
+
+}
---
+++ src/testpackage/testclass.java
@@ -0,0 +1,13 @@
+package testpackage;
+
+import org.testng.annotations.Test;
+
+public class testclass {
+
+@Test
+	public  void test() {
+		System.out.println("Test");
+
+	}
+
+}
